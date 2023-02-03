8af15780b925693599175a43e852d27c79506487
gideonclottey <gideonclotteyint@gmail.com>
2023-02-03 00:12:30 +0000

    restructuring of api and populating the stock table with one company

--- assets/js/stock.js
+++ assets/js/stock.js
@@ -1,18 +1,49 @@
-const apiKeyStock = 'KBINZE6ALWKKIC1E';
+var  companies = ['Apple','zm','GOOGL','BAC','AMS','THYAO']
+for(i =0; i< companies.length; i++){
 
-let symbol = 'IBM';
+   
+    //console.log(comp_symbol)
+    var settings = {
+	
 
-const urlStockQuery ='https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol='+ symbol +'&interval=5min&apikey='+ apiKeyStock 
+        "async": true,
+        
+        "crossDomain": true,
+        "url": "https://real-time-finance-data.p.rapidapi.com/search?query="+ companies[i],
+        "method": "GET",
+        "headers": {
+            "X-RapidAPI-Key": "9012d869e8msha9b24fd1c904769p1eb313jsn0552cf00b53c",
+            "X-RapidAPI-Host": "real-time-finance-data.p.rapidapi.com"
+        }
+    };
+}
 
-$.ajax({ url: urlStockQuery})
-    // once ajax response received
-    .then(function (response) {
-        console.log(response)
 
-        console.log(response['Time Series (5min)'])
+$.ajax(settings).done(function (response) {
+	//console.log(response.data.stock[0].price);
+    var datas =response.data.stock
+    for(i =0; i < 10; i++){
+        const data = datas[i]
+
+        console.log(data)
+
+        
+        var  stock =$('<tr>');
+        var stock_head = $('<th>')
+            stock_head.text(data.symbol)
+        var name = $('<th>')
+            name.text(data.name)
+        var price = $('<th>')
+            price.text(data.price)
+        var change = $('<th>')
+            change.text(data.change)
+        var change_percentage = $('<th>')
+            change_percentage.text(data.change_percent)
+      
+        $(stock).append(stock_head,name,change,change_percentage) 
+        $('#stock-container').append(stock)   
         
 
+    }
 
-
-
-    })
+});
